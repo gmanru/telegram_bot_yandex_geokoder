d93ee5e24bd0aef29f459abf2df0af66bb409c76
Gman <mrgman11@yandex.ru>
2020-09-20 10:48:23 +0300

    update

--- search.py
+++ search.py
@@ -3,7 +3,7 @@ with open('secret.txt') as f:
     geokoder_api_key = f.read().strip()
 
 
-filters = ['Орловская область','Москва']
+filters = ['Елец','Москва','Россия']
 list_of_adresses =[]
 
 def get_coord(addres):
@@ -11,7 +11,7 @@ def get_coord(addres):
         'https://geocode-maps.yandex.ru/1.x/?format=json&apikey={}&geocode={}'.format(geokoder_api_key, addres))
     d = dict(a.json())
     try:
-        point = d['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'].split(' ')
+        #dipoint = d['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'].split(' ')
         results = d['response']['GeoObjectCollection']['metaDataProperty']['GeocoderResponseMetaData']['results']
         for adres in range(int(results)):
             list_of_adresses.append(d['response']['GeoObjectCollection']['featureMember'][adres]['GeoObject']['metaDataProperty']['GeocoderMetaData']['text'])
@@ -19,38 +19,16 @@ def get_coord(addres):
 
     except IndexError:
         return [None, None]
-    coord = list(map(float, point))
+    #coord = list(map(float, point))
     for adres in range(len(list_of_adresses)):
         for filter in range(len(filters)):
             if filters[filter] in list_of_adresses[adres]:
-                print(list_of_adresses[adres],results)
-        #print(filters[filter])
-        #print(list_of_adresses)
-    #coord.reverse()
-    #print(coord)
+                #print(list_of_adresses[adres])
+                pass
     return list_of_adresses
 
 
-def get_adres(coord:str):
-    #print('entered')
-    a = requests.get(
-        'https://geocode-maps.yandex.ru/1.x/?format=json&apikey={}&geocode={}'.format(geokoder_api_key, coord))
-    d = dict(a.json())
-    #print(d)
-    try:
-        results = d['response']['GeoObjectCollection']['metaDataProperty']['GeocoderResponseMetaData']['results']
-        for adres in range(len(results)):
-            #list_of_adresses.append(d['response']['GeoObjectCollection']['featureMember'][adres]['GeoObject']['metaDataProperty']['GeocoderMetaData']['text'])
-            print(d['response']['GeoObjectCollection']['featureMember'][adres]['GeoObject']['metaDataProperty']['GeocoderMetaData']['text'])
-    except IndexError:
-        return [None, None]
-    #coord = list(map(float, point))
-    #coord.reverse()
-    #for filter in range(len(filters)):
-    if filters in list_of_adresses:
-        print(adres,results)
 
-    return adres
 
-get_coord('Красная площадь')
+#get_coord('Красная площадь')
 #get_adres('37.621085, 55.753585')
